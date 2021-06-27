c4e4bb061dd50a2c91c7e0e83ac264222243a0e7
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2021-06-27 13:37:59 +0400

    Feat: Created createItem.js and added function into that

--- src/scripts/createItem.js
+++ src/scripts/createItem.js
@@ -6,16 +6,16 @@ function createItems(items) {
 
   items.forEach((item) => {
     const itemSection = `
-    <div class="counties-list__item">
-      <div class="item__title">
-        <div class="item__title__name">
-          <img src="${item.flag}" alt="${item.name.toLowerCase()}-flag" />
-          <h3><a href="https://en.wikipedia.org/wiki/${
-            item.name
-          }" target="_blank">${item.name}</a></h3>
+      <div class="counties-list__item">
+        <div class="item__title">
+          <div class="item__title__name">
+            <img src="${item.flag}" alt="${item.name.toLowerCase()}-flag" />
+            <h3><a href="https://en.wikipedia.org/wiki/${
+              item.name
+            }" target="_blank">${item.name}</a></h3>
+          </div>
         </div>
       </div>
-    </div>
     `
 
     wrapperItems.insertAdjacentHTML('beforeend', itemSection)
