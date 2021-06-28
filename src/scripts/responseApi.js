b42e04b928826ff1dc567cdb0b8fb76d12f4a064
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2021-06-28 15:47:21 +0400

    Feat: Added loading

--- src/scripts/responseApi.js
+++ src/scripts/responseApi.js
@@ -1,11 +1,16 @@
 // URLs
 const allURL = 'https://restcountries.eu/rest/v2/all'
 const byNameURL = 'https://restcountries.eu/rest/v2/name/'
+// Map section
+const mapSection = document.getElementById('chart-div')
 
 window.onload = fetchFunc(allURL)
 
 // Fetch function
 function fetchFunc(url, response = '') {
+  mapSection.innerHTML =
+    '<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>'
+
   fetch(url + response)
     .then((response) => response.json())
     .then((data) => {
--- src/scripts/searchInput.js
+++ src/scripts/searchInput.js
@@ -1,8 +1,13 @@
 const searchInput = document.getElementById('search')
 
+let myTime
+
 searchInput.addEventListener('input', (evt) => {
+  clearTimeout(myTime)
   const value = evt.target.value.trim()
 
-  if (value === '') fetchFunc(allURL)
-  else fetchFunc(byNameURL, value)
+  myTime = setTimeout(() => {
+    if (value === '') fetchFunc(allURL)
+    else fetchFunc(byNameURL, value)
+  }, 500)
 })
